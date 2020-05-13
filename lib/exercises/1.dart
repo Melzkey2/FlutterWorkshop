95efc1e5e82982e289016cdb678a175743e0be55
Melzkey2 <r.babasa09@gmail.com>
2020-05-13 15:52:49 +0800

    My first Commit

---
+++ lib/exercises/1.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class One extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Exercise 1", style: TextStyle(fontFamily: "GothamRndMedium")),
+      ),
+      body: Container(
+      	child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text("Hellow Mel", style: TextStyle(fontSize: 27, fontFamily: "GothamRndLight"),),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text("You made it", style: TextStyle(fontSize: 27, fontFamily: "GothamRndBook"),),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text("first", style: TextStyle(fontSize: 27, fontFamily: "GothamRndMedium"),),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text("Hello World", style: TextStyle(fontSize: 27, fontFamily: "GothamRndBold"),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
